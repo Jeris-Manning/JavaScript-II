8600dc04b32bb90cc54fa8831d38bb4aecab6ff1
Jeris Manning <the.goose.moose@gmail.com>
2019-05-26 17:38:42 -0600

    Solves all challenges except for array-methods 5.2, 5.3 and callbacks stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,88 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function bestColor() {
+  
+  function sayFavoriteColor(faveColor) {
+    console.log(`My favorite color is ${faveColor}`);
+  }
+  return sayFavoriteColor;
+}
 
+//Classic people on the street and their favorite colors test
+
+// let donsColor = bestColor();
+// let petesColor = bestColor();
+// let horaceColor = bestColor();
+
+// petesColor("Like SUPER bright pink");
+// donsColor("origin");
+// horaceColor("Darkness");
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
-
 // ==== Challenge 2: Create a counter function ====
+// Return a function that when invoked increments and returns a counter variable.
+
 const counter = () => {
-  // Return a function that when invoked increments and returns a counter variable.
+  let clicker = 0;
+  let clickCount = () => {
+    clicker++;
+    console.log(clicker);
+    return clicker;
+  };
+  return clickCount;
 };
+
+// Classic Mouse and Moose counting test
+
+// const miceCanCount = counter();
+// const mooseCanCount = counter();
+// mooseCanCount();
+// mooseCanCount();
+// mooseCanCount();
+// mooseCanCount();
+// mooseCanCount();
+// miceCanCount();
+// mooseCanCount();
+// miceCanCount();
+// miceCanCount();
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
+// Return an object that has two methods called `increment` and `decrement`.
+// `increment` should increment a counter variable in closure scope and return it.
+// `decrement` should decrement the counter variable and return it.
+
 const counterFactory = () => {
-  // Return an object that has two methods called `increment` and `decrement`.
-  // `increment` should increment a counter variable in closure scope and return it.
-  // `decrement` should decrement the counter variable and return it.
+  let count = 0;
+  const counter = {
+    increment: function() {
+      count++;
+      console.log(count);
+      return count;
+    },
+    decrement: function() {
+      count--;
+      console.log(count);
+      return count;
+    }
+  };
+  return counter;
 };
+
+// Test robot looking for closure. Going to let him test things out. 
+
+// const clickerBot = counterFactory();
+
+// clickerBot.increment();
+// clickerBot.increment();
+// clickerBot.increment();
+// clickerBot.increment();
+// clickerBot.increment();
+// clickerBot.decrement();
+// clickerBot.decrement();
+// clickerBot.increment();
+// clickerBot.decrement();
